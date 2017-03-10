6d9f7811f9f221ca54c7a0bc37ce2b21f0156cdc
123 <2228312644@qq.com>
2017-03-10 20:44:52 +0800

    更换模板

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var UserInfo = require('../models/userInfo');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -44,17 +44,17 @@ router.get('/task', function(req, res, next) {
 router.get('/taskDetail', function(req, res, next) {
   res.render('taskDetail', { title: 'Express' });
 });
-//������ģ�����·�ɣ�ȷ���ý�����ɾ��
-router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'services' });
+
+router.get('/typo', function(req, res, next) {
+  res.render('typo', { title: 'typo' });
 });
-router.get('/typography', function(req, res, next) {
-  res.render('typography', { title: 'typography' });
+router.get('/gallery', function(req, res, next) {
+  res.render('gallery', { title: 'gallery' });
 });
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'contact' });
 });
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'contact' });
+  res.render('about', { title: 'about' });
 });
 module.exports = router;
